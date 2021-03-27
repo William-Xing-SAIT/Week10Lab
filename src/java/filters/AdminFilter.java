9d3a13e1204ec4eaa5ab00af804d13206bf53fe4
814545 <814545@SAIT225651IT.ACDM.DS.SAIT.CA>
2021-03-27 01:02:46 -0600

    Unused imports removed.

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -2,9 +2,6 @@ package filters;
 
 import dataaccess.UserDB;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
